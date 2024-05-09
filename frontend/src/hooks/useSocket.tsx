755087c916f42955ab66244a592d6bd8620cb1fd
Piyush <sainipiyush8860@gmail.com>
2024-05-09 20:36:58 +0530

    fixed the env

--- frontend/src/hooks/useSocket.tsx
+++ frontend/src/hooks/useSocket.tsx
@@ -5,7 +5,7 @@ export const useSocket = ()=>{
     const [socket, setSocket] = useState<WebSocket | null>(null)
 
 useEffect(() => {
-  const ws = new WebSocket( "ws://localhost:8080")
+  const ws = new WebSocket( "wss://chess-1f07.onrender.com/")
      setSocket(ws)
 
      ws.onclose = (()=>{
